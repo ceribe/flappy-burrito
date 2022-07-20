106960e90ae6786a7ae2b0fa0d53a0f25493a6ba
Ceribe <ceribekagami@gmail.com>
2022-07-20 21:41:59 +0200

    Add gui and score

--- game_state.lua
+++ game_state.lua
@@ -1,6 +1,12 @@
+GS_BEFORE_START = "0"
+GS_PLAYING = "1"
+GS_PLAYER_DIED = "2"
+
 local M = {}
 M.game_speed_multiplier = 1
 M.game_speed = -240
+M.state = GS_BEFORE_START
 M.remaining_cheese_time = 0
 M.remaining_chilli_time = 0
+M.score = 0
 return M
