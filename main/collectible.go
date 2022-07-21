283840720ff8ab8ed08028f925adbc0050c5c98e
Ceribe <ceribekagami@gmail.com>
2022-07-21 17:48:15 +0200

    Add particles to collectibles

--- main/collectible.go
+++ main/collectible.go
@@ -13,6 +13,21 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "particle"
+  component: "/main/particle.particlefx"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 1.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "sprite"
   type: "sprite"
